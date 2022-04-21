2fcec7a89a1b2f35c53cb0461db0940e24c69799
Adrià Prats Torres <104071686+AdriaPrats@users.noreply.github.com>
2022-04-21 16:32:17 +0200

    Update CleanText.jsx

--- CleanText.jsx
+++ CleanText.jsx
@@ -6,50 +6,7 @@
 
 var active_doc = app.activeDocument;  
   
-var search_string = ";";
-var replace_string = "";  
-  
-var text_frames = active_doc.textFrames;  
-  
-if (text_frames.length > 0)  
-{  
-    for (var i = 0 ; i < text_frames.length; i++)  
-      {  
-          var this_text_frame = text_frames[i];  
-           var new_string = this_text_frame.contents.replace(search_string, replace_string);  
-             
-           if (new_string != this_text_frame.contents)  
-               {  
-                    this_text_frame.contents = new_string;  
-               }  
-      }  
-} 
-
-
-var active_doc = app.activeDocument;  
-  
-var search_string = "|";
-var replace_string = "";  
-  
-var text_frames = active_doc.textFrames;  
-  
-if (text_frames.length > 0)  
-{  
-    for (var i = 0 ; i < text_frames.length; i++)  
-      {  
-          var this_text_frame = text_frames[i];  
-           var new_string = this_text_frame.contents.replace(search_string, replace_string);  
-             
-           if (new_string != this_text_frame.contents)  
-               {  
-                    this_text_frame.contents = new_string;  
-               }  
-      }  
-} 
-
-var active_doc = app.activeDocument;  
-  
-var search_string = "|";
+var search_string = /;|\|/gi;;
 var replace_string = "";  
   
 var text_frames = active_doc.textFrames;  
